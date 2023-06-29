67786809620b0eaecbd78f836f73aa37a0cc40ae
vyacheslavsafin <safin.vr@mail.ru>
2023-06-30 02:03:59 +0300

    добвлен атрибут экземпляра класса реализован класс Channel, метод print_info

--- src/channel.py
+++ src/channel.py
@@ -1,12 +1,21 @@
+import json
+import os
+
+# необходимо установить через: pip install google-api-python-client
+from googleapiclient.discovery import build
+api_key: str = os.getenv('YT_API_KEY')
+youtube = build('youtube', 'v3', developerKey=api_key)
 
 
 class Channel:
-    """Класс для ютуб-канала"""
+    """Класс для ютуб-канала
+"""
 
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        pass
+        self.channel_id = channel_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        pass
+        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
+        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
