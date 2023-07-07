ca6d4cae81271492f99f51aa8ffc43e62930c179
vyacheslavsafin <safin.vr@mail.ru>
2023-07-07 04:45:21 +0300

    добавлены новые атрибуты экземпляра добавлены два метода в класс Channel

--- src/channel.py
+++ src/channel.py
@@ -3,19 +3,55 @@ import os
 
 # необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
-api_key: str = os.getenv('YT_API_KEY')
-youtube = build('youtube', 'v3', developerKey=api_key)
+api_key = os.getenv('YT_API_KEY')
 
 
 class Channel:
-    """Класс для ютуб-канала
-"""
+    """
+    Класс для ютуб-канала
+    """
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, channel_id: str) -> None:
+    def __init__(self, channel: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
-        self.channel_id = channel_id
+        youtube = self.get_service()
+        data = youtube.channels().list(id=channel, part='snippet,statistics').execute()
+
+        self.__channel_id = data['items'][0]['id']
+        self.title = data['items'][0]['snippet']['title']
+        self.description = data['items'][0]['snippet']['description']
+        self.url = 'https://www.youtube.com/channel/' + self.__channel_id
+        self.subscriber_count = data['items'][0]['statistics']['subscriberCount']
+        self.video_count = data['items'][0]['statistics']['videoCount']
+        self.view_count = data['items'][0]['statistics']['viewCount']
+
+    @property
+    def channel_id(self):
+        return self.__channel_id
+
+    @channel_id.setter
+    def channel_id(self, new_id):
+        self.__channel_id = new_id
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
-        dict_to_print = youtube.channels().list(id=self.channel_id, part='snippet,statistics').execute()
-        print(json.dumps(dict_to_print, indent=2, ensure_ascii=False))
+        youtube = self.get_service()
+        channel = youtube.channels().list(id=self.__channel_id, part='snippet,statistics').execute()
+        print(json.dumps(channel, indent=2, ensure_ascii=False))
+
+    @classmethod
+    def get_service(cls):
+        return cls.youtube
+
+    def to_json(self, json_file):
+        dict_ = {
+            'id': self.__channel_id,
+            'title': self.title,
+            'description': self.description,
+            'url': self.url,
+            'subscriber_count': self.subscriber_count,
+            'video_count': self.video_count,
+            'view_count': self.video_count,
+        }
+        with open(json_file, 'w') as file:
+            file.write(json.dumps(dict_, indent=2, ensure_ascii=False))
