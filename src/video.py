7a9ab9b710d43df619149547fbbce7eb0849ba10
vyacheslavsafin <safin.vr@mail.ru>
2023-07-24 01:45:51 +0300

    добавлены блоки try, except

--- src/video.py
+++ src/video.py
@@ -7,13 +7,20 @@ class Video:
     def __init__(self, video_id):
         self.video_id = video_id
         youtube = Channel.get_service()
-        video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
-                                               id=self.video_id
-                                               ).execute()
-        self.title = video_response["items"][0]["snippet"]["title"]
-        self.url = "https://www.youtube.com/watch?v=" + self.video_id
-        self.view_count = video_response["items"][0]["statistics"]["viewCount"]
-        self.statistic_count = video_response["items"][0]["statistics"]["likeCount"]
+        try:
+            video_response = youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                                   id=self.video_id
+                                                   ).execute()
+            self.title = video_response["items"][0]["snippet"]["title"]
+            self.url = "https://www.youtube.com/watch?v=" + self.video_id
+            self.view_count = video_response["items"][0]["statistics"]["viewCount"]
+            self.like_count = video_response["items"][0]["statistics"]["likeCount"]
+        except IndexError:
+            self.video_id = video_id
+            self.title = None
+            self.url = None
+            self.view_count = None
+            self.like_count = None
 
     def __str__(self):
         return self.title
